6e52021f46f6c65efc9dda3facd21caf8dfce9c2
Phuong <ttp21197@gmail.com>
2020-04-07 11:32:50 +0700

    day len github

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form action="/form.php" method="POST">
+  <p> Ho Ten: <input type="text" name="fname" value="Phuong"
+//PhuongB1609542
+  <p> Email: <input type="text" name="mail" value=""></p>
+  <input type="submit" name="submit" value="submit">
+</form> 
+<br><a href = setup.php>Create Database</a>
+<br><a href = add_account.php>Add Account</a>
+<br><a href = list_account.php>List Accounts </a>
+</body>
+</html>
